0e76541147e464478676e56544230541e06d06cb
jeltjebvellema <jeltjebvellema@students.abtech.edu>
2021-11-19 00:14:58 -0500

    continued to adapt table to call appropriate column names in while loop

--- public/salamanders/index.php
+++ public/salamanders/index.php
@@ -5,7 +5,7 @@
 
 $salamander_set = find_all_salamanders();
 
-/*
+/* remove this array, now retrieving via db
   $salamanders = [
     ['id' => '1', 'position' => '1', 'visible'  => '1', 'type'  => 'Red-legged Salamander'],
     ['id' => '2', 'position' => '2', 'visible'  => '1', 'type'  => 'Pigeon Mountain Salamander'],
@@ -30,26 +30,27 @@ $salamander_set = find_all_salamanders();
       <table class="list"
         <tr>
           <th>ID</th>
-          <th>Position</th>
-          <th>Visible</th>
-          <th>Type</th>
+          <th>Name</th>
+          <th>Habitat</th>
+          <th>Description</th>
           <th colspan = "3">Actions</th>
         </tr>
 
         <?php while($salamander = mysqli_fetch_assoc($salamander_set)) { ?>
           <tr>
             <td><?php echo h($salamander['id']); ?></td>
-            <td><?php echo h($salamander['position']); ?></td>
-            <td><?php echo $salamander['visible'] == 1 ? 'true' : 'false'; ?></td>
-            <td><?php echo h($salamander['type']); ?></td>
-            <td><a class ="action" href="<?= url_for('salamanders/show.php?type=' .  h(u($salamander['type'])). '&id=' .h(u($salamander['id']))); ?>">
+            <td><?php echo h($salamander['name']); ?></td>
+            <td><?php echo $salamander['habitat']?></td>
+            <td><?php echo h($salamander['description']); ?></td>
+            <td><a class ="action" href="<?= url_for('salamanders/show.php?name=' .  h(u($salamander['name'])). '&id=' .h(u($salamander['id']))); ?>">
               View</a></td>
-            <td><a class="action" href="<?= url_for('salamanders/edit.php?type=' .  h(u($salamander['type'])). '&id=' .h(u($salamander['id']))); ?>">Edit</a></td>
+            <td><a class="action" href="<?= url_for('salamanders/edit.php?name=' .  h(u($salamander['name'])). '&id=' .h(u($salamander['id']))); ?>">Edit</a></td>
             <td><a class ="action" href="">Delete</a></td>
         </tr>     
         <?php } ?>
         </table>
     </div>
+    <cite>Thanks to <a href="https://herpsofnc.org/"> Amphibians and Reptiles of NC </a></cite>
   </div>
 
   <?php include(SHARED_PATH. '/salamanderfooter.php');?>
