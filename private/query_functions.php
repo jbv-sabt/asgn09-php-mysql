867f4b8f7b887db0c550221b2dd64e50ed9f3f0d
jeltjebvellema <jeltjebvellema@students.abtech.edu>
2021-11-19 22:11:08 -0500

    added oneline echo sql statement for troubleshooting

--- private/query_functions.php
+++ private/query_functions.php
@@ -1,8 +1,10 @@
 <?php
 function find_all_salamanders() {
     global $db;
-    $sql = "SELECT * FROM salamander ";
+    $sql = "SELECT * FROM salamander git s";
     $sql .= "ORDER BY name ASC";
+    // troubleshooting code commented out
+    //echo $sql;
     $result = mysqli_query($db, $sql);
     confirm_result_set($result);
     return $result;
