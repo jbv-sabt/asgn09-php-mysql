80d0ae79e69cd49c99971eff511e3c6709170f64
jeltjebvellema <jeltjebvellema@students.abtech.edu>
2021-11-21 16:21:01 -0500

    removed "+7" from $public_end for deployment

--- private/initialize.php
+++ private/initialize.php
@@ -15,14 +15,14 @@
   //define("WWW_ROOT", '/public');
   // define("WWW_ROOT", '');
   // * Can dynamically find everything in URL up to "/public"
-  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') +7;
+  $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public');
   $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
   define("WWW_ROOT", $doc_root);
 
   require_once('functions.php');
   require_once('database.php');
   require_once('query_functions.php');
-
+ 
   $db = db_connect();
 
 ?>
